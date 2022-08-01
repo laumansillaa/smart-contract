9f8726884646c7af967ff999130c1170a3345829
Lautaro Mansilla <85461971+laumansillaa@users.noreply.github.com>
2022-08-01 17:15:39 -0300

    update app.jsx - add function getAllWaves - styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from "react";
-import { Box, Container, Button } from "@mui/material";
+import { Box, Container, Button, Card, CardContent, Typography, TextField} from "@mui/material";
 import { ethers } from "ethers";
 import abi from "./utils/WavePortal.json";
 import { smartContractAddress } from "./address.js";
 import "./App.css";
+import './styles/card.css';
+import './styles/search.css'
 
 function App() {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [allWaves, setAllWaves] = useState([])
+  const [countWave, setCountWave] = useState(0)
+  
   const contractAddress = smartContractAddress;
   const contractABI = abi.abi;
+  // console.log('COUNTWAVE', countWave)
+
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -28,6 +35,7 @@ function App() {
         const account = accounts[0];
         console.log("Found an authorized account: ", account);
         setCurrentAccount(account);
+        await getAllWaves()
       } else {
         console.log("No authorized account found");
       }
@@ -72,13 +80,15 @@ function App() {
         let count = await wavePortalContract.getTotalWaves();
         console.log("Retrieved total wave count...", count.toNumber());
 
-        const waveTxn = await wavePortalContract.wave();
+        const waveTxn = await wavePortalContract.wave('This is a message');
         console.log('Mining...', waveTxn.hash)
 
         await waveTxn.wait();
         console.log('Mined -- ', waveTxn.hash)
 
         count = await wavePortalContract.getTotalWaves()
+        setCountWave(count.toNumber())
+        
         console.log('Retrieved total wave count...', count.toNumber())
 
       } else {
@@ -89,23 +99,112 @@ function App() {
     }
   };
 
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer) 
+
+        //call the getAllWaves method 
+        const waves = await wavePortalContract.getAllWaves();
+
+        // we only need address, timestamp, and message in our UI so let's
+        // pick those out
+
+        let wavesCleaned = []
+        waves.forEach(wave => {
+          wavesCleaned.push({
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp *1000),
+            message: wave.message
+          })
+        })
+
+        setAllWaves(wavesCleaned)
+      } else {
+        console.log("Ethereum object doesn't exist!!")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
   useEffect(() => {
     checkIfWalletIsConnected();
-  }, []);
+  }, [countWave]);
+
+
 
   return (
     <Container className="App">
-      <h1>Hi! I'm laumansilla, welcome to my first smart-contract</h1>
-      <Box>
-        <Button variant="contained" disableElevation onClick={wave}>
+      <Box 
+        className='wallet-nav'
+      >
+        <img 
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/256px-Ethereum_logo_2014.svg.png"
+          alt="Ethereum-logo"
+          className='img-nav'
+        />
+        <Box className='wallet-contain-button'>
+          {/* If there is no currentAccount render this button */}
+          {!currentAccount? 
+            <>              
+              <h3 
+                className='wallet-nav-title'
+              >Hey you still haven't connected your wallet!</h3>
+              <Button 
+                variant="contained"
+                onClick={connectWallet}
+                sx={{ml: 2, mr:2}}
+              >Connect Wallet
+              </Button>
+            </> :
+            <>
+              <h3 className='wallet-nav-title'>Yes now! Very well, you are now connected.</h3>
+            </>
+          }
+        </Box>
+      </Box>
+      <Box 
+        className='contain-body'
+        
+        >
+        <h1
+          className='body-title'
+        >Hi! Welcome to my first smart-contract</h1>
+        <Button 
+          variant="contained" 
+          disableElevation 
+          onClick={wave}
+          sx={{mb: 2}}
+          >
           Send me a wave
         </Button>
       </Box>
-      <Box sx={{ mt: 2 }}>
-        {/* If there is no currentAccount render this button */}
-        {!currentAccount && (
-          <Button onClick={connectWallet}>Connect Wallet</Button>
-        )}
+      <Box className='contain-search' >
+        <Box>
+          <h3 className='count-wave'>Retrieved total wave count: {countWave}</h3>
+        </Box>
+        <Box>
+          <TextField label='Insert you Address' variant='outlined' className='search'/>
+        </Box>
+      </Box>
+      <Box className='containCard'>
+        {allWaves.map((wave, index) => {
+          return (
+            <Card key={index} sx={{width:600}} className='card'>
+              <CardContent>
+                <Typography sx={{ fontSize: 20 }} className='card-title'>New message!</Typography>
+                <Typography color="text.secondary" className='card-subtitle'>Address: {wave.address}</Typography>
+                <Typography color="text.secondary" className='card-subtitle'>Time: {wave.timestamp.toString()}</Typography>
+                <Typography className='card-subtitle'>Message: {wave.message}</Typography>
+              </CardContent>
+            </Card>
+          )
+        })}
       </Box>
     </Container>
   );
