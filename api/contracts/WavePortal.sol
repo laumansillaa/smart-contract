91d14b7e7906e17dc5d2f4a277ef8d3404bca7e7
Lautaro Mansilla <85461971+laumansillaa@users.noreply.github.com>
2022-08-01 17:13:48 -0300

    update smart-contract

--- api/contracts/WavePortal.sol
+++ api/contracts/WavePortal.sol
@@ -7,13 +7,39 @@ contract WavePortal {
 
     //Variable de estado, se almacena de forma permanente en el almacenamiento por contrato.
 
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    // A struct is basically a custom datatype where we can customize what we want
+    // to hold inside it.
+
+    struct Wave {
+        address waver; //The address of the user who waved
+        string message; //The message the user sent
+        uint256 timestamp; //The timestamp when the user waved
+    }
+
+    // I declare a variable waves that lets me store an array of structs.
+    // This is what lets me hold the waves anyone ever sends to me!
+
+    Wave[] waves;
+
     constructor() {
         console.log("Hi!! I'm a contract");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s has waved!", msg.sender); //La persona que llamo a la funcion;
+        console.log("%s has waved!", msg.sender, _message); //La persona que llamo a la funcion;
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    //which will return the struct array waves, to us.
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
--- api/scripts/run.js
+++ api/scripts/run.js
@@ -1,43 +1,39 @@
-const value = async (val) => {
-    const allRemitent = [];
-    if (!allRemitent.includes(val)) {
-        allRemitent.push(val)
-    }
-    
-    console.log('FUNCTION VALUE', allRemitent)
-}
-
-
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
+    //const [owner, randomPerson] = await hre.ethers.getSigners();
     // console.log('OWNER', owner)
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy(); // Hardhat -> redlocal, que se destruye terminado el script
     //cada vez que se ejecute el contrato, se crea una cadena nueva.
     await waveContract.deployed();
     console.log('Contract deployed to: ', waveContract.address);
-    console.log('Contract deployed by: ', owner.address)
+    // console.log('Contract deployed by: ', owner.address)
 
     // Hardhat Runtime Environment, o HRE para abreviar, es un objeto que contiene toda la funcionalidad que 
     // Hardhat expone cuando ejecuta una tarea, prueba o script. En realidad, Hardhat es el HRE.
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
+    console.log(waveCount.toNumber())
 
-    let waveTxn = await waveContract.wave();
+    let waveTxn = await waveContract.wave('A message!!');
+    await waveTxn.wait(); //wait for the transaction to be mined
+
+    const [_, randomPerson] = await hre.ethers.getSigners();
+    waveTxn = await waveContract.connect(randomPerson).wave('Another message!!')
+
     await waveTxn.wait();
 
-    waveCount = await waveContract.getTotalWaves();
+    // waveCount = await waveContract.getTotalWaves();
 
-    waveTxn = await waveContract.connect(randomPerson).wave();
+    // waveTxn = await waveContract.connect(randomPerson).wave();
     // console.log('waveContract.connecttt ------- ', waveTxn)
 
-
-    await waveTxn.wait();
+    let allWaves = await waveContract.getAllWaves();
+    console.log('allWaves', allWaves)
 
     
-    waveCount = await waveContract.getTotalWaves();
+    // waveCount = await waveContract.getTotalWaves();
 
-    await value(waveTxn.from);
+   
 
 };
 
