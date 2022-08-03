cbb2e1b471aa5487ffeb3489c386f8f8ebf79617
Lautaro Mansilla <85461971+laumansillaa@users.noreply.github.com>
2022-08-03 12:33:58 -0300

    update smart-contract

--- api/contracts/WavePortal.sol
+++ api/contracts/WavePortal.sol
@@ -4,7 +4,7 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
-
+    uint256 private seed;
     //Variable de estado, se almacena de forma permanente en el almacenamiento por contrato.
 
     event NewWave(address indexed from, uint256 timestamp, string message);
@@ -23,16 +23,42 @@ contract WavePortal {
 
     Wave[] waves;
 
-    constructor() {
+    mapping(address => uint256) public lastWavedAt;
+
+    constructor() payable {
         console.log("Hi!! I'm a contract");
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(string memory _message) public {
+        // 30 seconds
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15m"
+        );
+
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
         console.log("%s has waved!", msg.sender, _message); //La persona que llamo a la funcion;
 
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+        console.log("Random # generated: %d", seed);
+
+        if (seed < 50) {
+            console.log("is won", msg.sender);
+            //The same code we had before to send the prize.
+            //Send Eth
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
         emit NewWave(msg.sender, block.timestamp, _message);
     }
 
--- api/scripts/run.js
+++ api/scripts/run.js
@@ -2,11 +2,23 @@ const main = async () => {
     //const [owner, randomPerson] = await hre.ethers.getSigners();
     // console.log('OWNER', owner)
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy(); // Hardhat -> redlocal, que se destruye terminado el script
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther('0.1')
+    }); // Hardhat -> redlocal, que se destruye terminado el script
     //cada vez que se ejecute el contrato, se crea una cadena nueva.
     await waveContract.deployed();
-    console.log('Contract deployed to: ', waveContract.address);
+    console.log('Contract addy: ', waveContract.address);
     // console.log('Contract deployed by: ', owner.address)
+
+    /*
+    * Get contract balance
+    */
+
+    let contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+
+    console.log('Contract balance: ',
+    hre.ethers.utils.formatEther(contractBalance))
+
 
     // Hardhat Runtime Environment, o HRE para abreviar, es un objeto que contiene toda la funcionalidad que 
     // Hardhat expone cuando ejecuta una tarea, prueba o script. En realidad, Hardhat es el HRE.
@@ -14,13 +26,29 @@ const main = async () => {
     waveCount = await waveContract.getTotalWaves();
     console.log(waveCount.toNumber())
 
-    let waveTxn = await waveContract.wave('A message!!');
-    await waveTxn.wait(); //wait for the transaction to be mined
+    // let waveTxn = await waveContract.wave('A message!!');
+    // await waveTxn.wait(); 
+    //wait for the transaction to be mined
 
-    const [_, randomPerson] = await hre.ethers.getSigners();
-    waveTxn = await waveContract.connect(randomPerson).wave('Another message!!')
+    // const [_, randomPerson] = await hre.ethers.getSigners();
+    // waveTxn = await waveContract.connect(randomPerson).wave('Another message!!')
 
+    // await waveTxn.wait();
+
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+        "Contract balance: ",
+        hre.ethers.utils.formatEther(contractBalance)
+    )
+
+    const waveTxn = await waveContract.wave('This is wave #1');
     await waveTxn.wait();
+
+    const waveTxn2 = await waveContract.wave('This is wave #2');
+    await waveTxn2.wait();
+
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log('Contract balance: ', hre.ethers.utils.formatEther(contractBalance))
 
     // waveCount = await waveContract.getTotalWaves();
 
